db87109e5b2169374bb918c456e543b135b074d1
alanypz <alanypz@gmail.com>
2018-08-12 14:42:34 -0700

    adding custom settings and packages

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'rest_framework',
     'snippets.apps.SnippetsConfig',
 ]
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Always use IPython for shell_plus
+SHELL_PLUS = "ipython"
+
+SHELL_PLUS_PRE_IMPORTS = [
+    ('pprint', 'pprint'),
+]
